e2db767e43fbf4c113caa3d00365f881e0d38871
Daniel Liu <xyfc128@gmail.com>
2024-06-11 00:53:25 +0800

    update script to get session string

--- telegram-bot/tests/get_session_string.py
+++ telegram-bot/tests/get_session_string.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 
-api_id = ""
-api_hash = ""
+api_id = int(input("api_id: "))
+api_hash = input("api_hash: ")
 
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
